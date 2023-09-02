ebb90ffbcb97f3c936f53881fa512077dd0c66b2
Bob Bytes <bytesbob@gmail.com>
2023-09-02 16:51:34 -0400

    finished game

--- script.js
+++ script.js
@@ -7,9 +7,39 @@ function computerChoice() {
 }
 
 function userChoice() {
+    let input;
     do {
         input = prompt("Enter choice").toLowerCase();
     } while(!options.includes(input))
     return input;
 }
-console.log(computerChoice());
+
+function getWinner(p1, p2) {
+    if (p1===p2) return 0;
+    else if (p1 === 'rock') return p2 === 'paper' ? 2 : 1;
+    else if (p1 === 'paper') return p2 === 'rock' ? 1 : 2;
+    else if (p1 === 'scissors') return p2 === 'rock' ? 2: 1;
+}
+
+function game() {
+    let player_score = 0;
+    let comp_score = 0;
+    for (let i = 0; i < 5; i++) {
+        comp = computerChoice();
+        player = userChoice();
+        let winner = getWinner(comp, player);
+        if (winner === 1){
+            console.log('computer won match');
+            comp_score++;
+        } else if (winner === 2) {
+            console.log('player won match');
+            player_score++;
+        } else console.log('match was a tie');
+        console.log(`Comp score: ${comp_score}, player score: ${player_score}`);
+    }
+    if (player_score > comp_score) console.log('Player wins!');
+    else if (player_score < comp_score) console.log('Computer wins!');
+    else console.log('The game was a tie!');
+}
+
+game();
