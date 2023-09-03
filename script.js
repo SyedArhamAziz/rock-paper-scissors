f95efa5206315ce7962bb91f0e159d839ad1e505
Bob Bytes <bytesbob@gmail.com>
2023-09-02 20:25:08 -0400

    added buttons and score indicators

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 const options = ['rock', 'paper', 'scissors'];
+const btns = document.querySelectorAll('button');
+const matchwinner = document.querySelector('#matchwinner');
+const scores = document.querySelector('#scores');
+console.log(matchwinner);
+
+btns.forEach((button) => button.addEventListener('click', playRound));
 
 function computerChoice() {
     const map = new Map([[0, 'rock'], [1, 'paper'], [2, 'scissors']]) 
     const v = Math.floor(3*Math.random());
     return map.get(v);
-}
-
-function userChoice() {
-    let input;
-    do {
-        input = prompt("Enter choice").toLowerCase();
-    } while(!options.includes(input))
-    return input;
 }
 
 function getWinner(p1, p2) {
@@ -21,12 +19,41 @@ function getWinner(p1, p2) {
     else if (p1 === 'scissors') return p2 === 'rock' ? 2: 1;
 }
 
+function playRound() {
+    if (rounds >= 5) return;
+    let comp= computerChoice();
+    let player= this.id;
+    let winner = getWinner(comp, player);
+    if (winner === 1){
+        console.log('computer won match');
+        comp_score++;
+    } else if (winner === 2) {
+        console.log('player won match');
+        player_score++;
+    } else console.log('match was a tie');
+    updateMatchResult(winner);
+    updateScores();
+    console.log(`Comp score: ${comp_score}, player score: ${player_score}`);
+    rounds++;
+}
+
+function updateMatchResult(result) {
+    let winner;
+    if (result === 1) winner = 'computer';
+    else if (result ===2) winner = 'player';
+    else winner = 'tie'; 
+
+    matchwinner.textContent = `Match winner: ${winner}`
+}
+
+function updateScores() {
+    scores.textContent = `Player score: ${player_score}, Computer score: ${comp_score}`;
+}
+
 function game() {
-    let player_score = 0;
-    let comp_score = 0;
-    for (let i = 0; i < 5; i++) {
-        comp = computerChoice();
-        player = userChoice();
+    do {
+        let comp = computerChoice();
+        let player = userChoice();
         let winner = getWinner(comp, player);
         if (winner === 1){
             console.log('computer won match');
@@ -36,10 +63,13 @@ function game() {
             player_score++;
         } else console.log('match was a tie');
         console.log(`Comp score: ${comp_score}, player score: ${player_score}`);
-    }
+        rounds++;
+    } while (rounds < 5)
     if (player_score > comp_score) console.log('Player wins!');
     else if (player_score < comp_score) console.log('Computer wins!');
     else console.log('The game was a tie!');
 }
 
-game();
+let rounds = 0;
+let player_score = 0;
+let comp_score = 0;
